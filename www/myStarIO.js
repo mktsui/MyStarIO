93600c69558714a06dbe7a74d42a2b40b90b7cfd
Ryan <mktsui@ymail.com>
2014-01-19 23:53:51 +0800

    Update myStarIO.js

--- www/myStarIO.js
+++ www/myStarIO.js
@@ -1,6 +1,6 @@
 var echo = function(str, callback) {
 	cordova.exec(callback, function(err) {
 		callback('Nothing to echo.');
-	}, "Echo", "echo", [str]);
+	}, "MyStarIO", "echo", [str]);
 };
 module.exports = echo;
