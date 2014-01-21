a72307266b1c44eb99ab831614e36a2dc776b873
Ryan <mktsui@ymail.com>
2014-01-21 21:25:21 +0800

    Delete searchStarP.js

--- www/searchStarP.js
+++
@@ -1,6 +0,0 @@
-var searchP = function(str, callback) {
-        cordova.exec(callback, function(err) {
-                callback('Cannot Search.');
-        }, "MyStarIO", "searchP", [str]);
-};
-module.exports = searchP;
