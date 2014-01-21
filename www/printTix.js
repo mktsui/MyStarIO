caf794c3791d62cb7d1ca50f7924c66b1db3f6b8
Ryan <mktsui@ymail.com>
2014-01-21 21:08:58 +0800

    Update printTix.js

--- www/printTix.js
+++ www/printTix.js
@@ -1,4 +1,4 @@
-var print = function(str, callback) {
+var print = function(str0, str1, callback) {
 	cordova.exec(callback, function(err) {
 		callback('Cannot Print.');
 	}, "MyStarIO", "print", [str0, str1]);
